860b147b6d6b0de91b9d5e01e161f96ad298a6db
ToF- <cthibaut@octo.com>
2016-07-06 16:34:32 +0200

    refactoring with term 0 = []

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -1,7 +1,7 @@
 module RomanNumerals where
 
 toRoman :: Int -> String
+toRoman 0         = ""
 toRoman n | n < 4 = replicate n 'I'
 toRoman 4         = "IV"
-toRoman 5         = "V"
-toRoman n | n < 9 = toRoman 5 ++ toRoman (n - 5)
+toRoman n | n < 9 = "V" ++ toRoman (n - 5)
