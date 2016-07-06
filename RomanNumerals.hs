23d82d0f7b99c8d9c224b7a31013edcacc161af3
ToF- <cthibaut@octo.com>
2016-07-06 17:07:28 +0200

    refactoring: generalize parseRoman

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -3,13 +3,13 @@ module RomanNumerals where
 toRoman :: Int -> String
 toRoman 0         = ""
 toRoman n | n < 4 = parseRoman n 1 "I"
-toRoman n | n < 5 = "IV" ++ toRoman (n `mod` 4)
+toRoman n | n < 5 = parseRoman n 4 "IV"
 toRoman n | n < 9 = parseRoman n 5 "V"
-toRoman n | n <10 = "IX" ++ toRoman (n `mod` 9)
+toRoman n | n <10 = parseRoman n 9 "IX"
 toRoman n | n <40 = parseRoman n 10 "X"
-toRoman n | n <50 = "XL" ++ toRoman (n `mod` 40)
+toRoman n | n <50 = parseRoman n 40 "XL"
 toRoman n | n <90 = parseRoman n 50 "L"
-toRoman n | n <100= "XC" ++ toRoman (n `mod` 90)        
+toRoman n | n <100= parseRoman n 90 "XC"
 toRoman n         = parseRoman n 100 "C"
 
 
