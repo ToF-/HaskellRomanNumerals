822ed09b63daf5fbf611d581ca4691573b436611
ToF- <cthibaut@octo.com>
2016-07-06 17:05:16 +0200

    refactoring: replace Char with String in parseRoman

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -2,16 +2,16 @@ module RomanNumerals where
 
 toRoman :: Int -> String
 toRoman 0         = ""
-toRoman n | n < 4 = parseRoman n 1 'I'
+toRoman n | n < 4 = parseRoman n 1 "I"
 toRoman n | n < 5 = "IV" ++ toRoman (n `mod` 4)
-toRoman n | n < 9 = parseRoman n 5 'V'
+toRoman n | n < 9 = parseRoman n 5 "V"
 toRoman n | n <10 = "IX" ++ toRoman (n `mod` 9)
-toRoman n | n <40 = parseRoman n 10 'X'
+toRoman n | n <40 = parseRoman n 10 "X"
 toRoman n | n <50 = "XL" ++ toRoman (n `mod` 40)
-toRoman n | n <90 = parseRoman n 50 'L'
+toRoman n | n <90 = parseRoman n 50 "L"
 toRoman n | n <100= "XC" ++ toRoman (n `mod` 90)        
-toRoman n         = parseRoman n 100 'C'
+toRoman n         = parseRoman n 100 "C"
 
 
-parseRoman n m c = replicate (n `div` m) c
+parseRoman n m s = concat (replicate (n `div` m) s)
                   ++ toRoman (n `mod` m)
