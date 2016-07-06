13ee4a1d365f08ceb249d3503795af9d891aef86
ToF- <cthibaut@octo.com>
2016-07-06 17:03:47 +0200

    refactoring: apply toRoman remainder also to 4 and 40

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -3,9 +3,9 @@ module RomanNumerals where
 toRoman :: Int -> String
 toRoman 0         = ""
 toRoman n | n < 4 = parseRoman n 1 'I'
-toRoman 4         = "IV"
+toRoman n | n < 5 = "IV" ++ toRoman (n `mod` 4)
 toRoman n | n < 9 = parseRoman n 5 'V'
-toRoman 9         = "IX"
+toRoman n | n <10 = "IX" ++ toRoman (n `mod` 9)
 toRoman n | n <40 = parseRoman n 10 'X'
 toRoman n | n <50 = "XL" ++ toRoman (n `mod` 40)
 toRoman n | n <90 = parseRoman n 50 'L'
