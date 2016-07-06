d698240e3197f8ecacb386a1cee2cf13a2ddb2ab
ToF- <cthibaut@octo.com>
2016-07-06 17:41:53 +0200

    refactoring: folding the parseRoman function

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -1,21 +1,22 @@
 module RomanNumerals where
 
 toRoman :: Int -> String
-toRoman n = fst $ toRoman' ("", n)
+toRoman n = fst $ foldl parseRoman ("", n) divisors
 
-toRoman' :: (String, Int) -> (String, Int)
-toRoman' (s,0)  = (s,0)
-toRoman' (s,n) | n < 4 = parseRoman (s,n) (1 ,"I")
-toRoman' (s,n) | n < 5 = parseRoman (s,n) (4 ,"IV")
-toRoman' (s,n) | n < 9 = parseRoman (s,n) (5 ,"V")
-toRoman' (s,n) | n <10 = parseRoman (s,n) (9 ,"IX")
-toRoman' (s,n) | n <40 = parseRoman (s,n) (10 ,"X")
-toRoman' (s,n) | n <50 = parseRoman (s,n) (40 ,"XL")
-toRoman' (s,n) | n <90 = parseRoman (s,n) (50 ,"L")
-toRoman' (s,n) | n <100= parseRoman (s,n) (90 ,"XC")
-toRoman' (s,n)         = parseRoman (s,n) (100, "C")
+divisors :: [(Int, String)]
+divisors =[
+           (100, "C")
+          ,(90 ,"XC")
+          ,(50 ,"L")
+          ,(40 ,"XL")
+          ,(10 ,"X")
+          ,(9 ,"IX")
+          ,(5 ,"V")
+          ,(4 ,"IV")
+          ,(1 ,"I")
+          ]
 
 
 parseRoman :: (String, Int) -> (Int,String) -> (String, Int)
-parseRoman (r,n) (m,s) = toRoman'
+parseRoman (r,n) (m,s) =
     (r ++ concat (replicate (n `div` m) s), n `mod` m)
