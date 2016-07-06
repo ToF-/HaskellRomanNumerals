a09608787edeb113c019af82eeffbe0d1c73d12f
ToF- <cthibaut@octo.com>
2016-07-06 16:55:51 +0200

    refactoring: n mod m instead of n - m

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -4,10 +4,10 @@ toRoman :: Int -> String
 toRoman 0         = ""
 toRoman n | n < 4 = parseRoman n 1 'I'
 toRoman 4         = "IV"
-toRoman n | n < 9 = "V" ++ toRoman (n - 5)
+toRoman n | n < 9 = "V" ++ toRoman (n `mod` 5)
 toRoman 9         = "IX"
 toRoman n | n <40 = parseRoman n 10 'X'
-toRoman n | n <50 = "XL" ++ toRoman (n - 40)
+toRoman n | n <50 = "XL" ++ toRoman (n `mod` 40)
                   
 toRoman n         = parseRoman n 100 'C'
 
