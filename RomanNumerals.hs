178fde436f67270f9f7673b5c1cec647f20db715
ToF- <cthibaut@octo.com>
2016-07-06 17:34:59 +0200

    refactoring: tuple for units and patterns

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -5,16 +5,17 @@ toRoman n = fst $ toRoman' ("", n)
 
 toRoman' :: (String, Int) -> (String, Int)
 toRoman' (s,0)  = (s,0)
-toRoman' (s,n) | n < 4 = parseRoman (s,n) 1 "I"
-toRoman' (s,n) | n < 5 = parseRoman (s,n) 4 "IV"
-toRoman' (s,n) | n < 9 = parseRoman (s,n) 5 "V"
-toRoman' (s,n) | n <10 = parseRoman (s,n) 9 "IX"
-toRoman' (s,n) | n <40 = parseRoman (s,n) 10 "X"
-toRoman' (s,n) | n <50 = parseRoman (s,n) 40 "XL"
-toRoman' (s,n) | n <90 = parseRoman (s,n) 50 "L"
-toRoman' (s,n) | n <100= parseRoman (s,n) 90 "XC"
-toRoman' (s,n)         = parseRoman (s,n) 100 "C"
+toRoman' (s,n) | n < 4 = parseRoman (s,n) (1 ,"I")
+toRoman' (s,n) | n < 5 = parseRoman (s,n) (4 ,"IV")
+toRoman' (s,n) | n < 9 = parseRoman (s,n) (5 ,"V")
+toRoman' (s,n) | n <10 = parseRoman (s,n) (9 ,"IX")
+toRoman' (s,n) | n <40 = parseRoman (s,n) (10 ,"X")
+toRoman' (s,n) | n <50 = parseRoman (s,n) (40 ,"XL")
+toRoman' (s,n) | n <90 = parseRoman (s,n) (50 ,"L")
+toRoman' (s,n) | n <100= parseRoman (s,n) (90 ,"XC")
+toRoman' (s,n)         = parseRoman (s,n) (100, "C")
 
 
-parseRoman (r,n) m s = toRoman'
+parseRoman :: (String, Int) -> (Int,String) -> (String, Int)
+parseRoman (r,n) (m,s) = toRoman'
     (r ++ concat (replicate (n `div` m) s), n `mod` m)
