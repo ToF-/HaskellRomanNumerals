f03eaf69dec825a362294594a1e38da25e427b8e
ToF- <cthibaut@octo.com>
2016-07-06 16:50:22 +0200

    refactoring: helper method

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -2,13 +2,14 @@ module RomanNumerals where
 
 toRoman :: Int -> String
 toRoman 0         = ""
-toRoman n | n < 4 = replicate (n `div` 1) 'I'
-                   ++ toRoman (n `mod` 1) 
+toRoman n | n < 4 = parseRoman n 1 'I'
 toRoman 4         = "IV"
 toRoman n | n < 9 = "V" ++ toRoman (n - 5)
 toRoman 9         = "IX"
-toRoman n | n <40 = replicate (n `div` 10) 'X' 
-                   ++ toRoman (n `mod` 10)
+toRoman n | n <40 = parseRoman n 10 'X'
                   
-toRoman n         = replicate (n `div` 100) 'C' 
-                   ++ toRoman (n `mod` 100)
+toRoman n         = parseRoman n 100 'C'
+
+
+parseRoman n m c = replicate (n `div` m) c
+                  ++ toRoman (n `mod` m)
